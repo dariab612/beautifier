6e2f0963537be81a4beac2a44e7a75ec12fd45e6
Daria <dariasb612@gmail.com>
2022-01-19 11:39:18 +0300

    static path

--- server/app.js
+++ server/app.js
@@ -56,7 +56,7 @@ app.use(cors(corsOptions));
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
-app.use(express.static(path.resolve('../client/build')));
+app.use(express.static(path.resolve('./client/build')));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 app.use(session(sessionConfig));
